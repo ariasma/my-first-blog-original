d6e3c492852a80ea2ae52d73824ffc3298182bf8
arima <ariasm_1@hotmail.com>
2015-05-29 16:18:52 -0400

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.post_list),
 	url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),
+	url(r'^post/new/$', views.post_new, name='post_new'),
+	url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 ]
 # assigning a view called post_list to ^$ URL. This
 # regular expression will match ^ (a beginning) followed by $(an end) - so 
